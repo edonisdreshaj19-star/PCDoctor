7ce88ed58629d8110f77582f3b51954b40182fb1
Donzi <Donzi@DESKTOP-PJ60GA6>
2026-03-27 22:11:46 +0100

    Projektdateien hinzufügen.

---
+++ PCDoctor.Core/SystemMonitor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace PCDoctor.Core
+{
+    class SystemMonitor
+    {
+        private PerformanceCounter cpuCounter;
+
+        public SystemMonitor() 
+        {
+            // CPU counter (Winbdows only)
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+
+            _ = cpuCounter.NextValue();
+            Thread.Sleep(500);
+        }
+
+        public SystemStats GetStats()
+        {
+            float cpu = GetCpuUsage();
+            (float totalRam, float availableRam) = GetMemoryInfo();
+
+            float usedRam = totalRam - availableRam;
+
+            return new SystemStats
+            {
+                CpuUsage = cpu,
+                TotalMemoryMB = totalRam,
+                AvailableMemoryMB = availableRam,
+                UsedMemoryMB = usedRam,
+            };
+        }
+
+        private (float totalRam, float availableRam) GetMemoryInfo()
+        {
+            try
+            {
+                return cpuCounter.NextValue();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private float GetCpuUsage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PCDoctor.Core/SystemStats.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PCDoctor.Core
+{
+    class SystemStats
+    {
+        public float CpuUsage { get; set; }
+        public float AvailableMemoryMB { get; set; }
+        public float TotalMemoryMB { get; set; }
+        public float UsedMemoryMB { get; set; }
+
+        public float RamUsage { get; set; }
+    }
+}
